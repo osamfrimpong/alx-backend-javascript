17df3ba35292e59acf141f14e6ed208b115c1fa4
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2024-04-12 04:22:31 +0000

    modified task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -11,27 +11,13 @@
 export default class Building {
   constructor(sqft) {
     if (new.target !== Building && this.evacuationWarningMessage === undefined) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    } else {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('Provide valid dimensions');
-      }
-      this._sqft = sqft;
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  set sqft(dimensions) {
-    if (typeof dimensions !== 'number') {
-      throw new TypeError('Provide valid dimensions');
-    }
-    this._sqft = dimensions;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
 }
+
