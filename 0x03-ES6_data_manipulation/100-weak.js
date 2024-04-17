44722f8177dea869a9bb323af058a8234d1ec0ea
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2024-04-17 05:32:26 +0000

    modified solution to advanced task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,8 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(endPoint) {
-  const total = weakMap.get(endPoint) || 0;
-  weakMap.set(endPoint, total + 1);
-  if (total >= 5) {
-    throw new Error('Endpoint load is high');
-  }
-  return total;
+export function queryAPI(endpoint) {
+  let called = 0;
+  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
+  weakMap.set(endpoint, called + 1);
+  if (called + 1 >= 5) throw new Error('Endpoint load is high');
 }
